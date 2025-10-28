2d1d158f4448c9c5753152710b696d3508133b72
anapau0 <159497193+anapau0@users.noreply.github.com>
2025-10-27 21:31:43 -0600

    Actualización del proyecto

---
+++ ProyectoPersonal/app/src/main/java/Controller/ProductController.kt
@@ -0,0 +1,33 @@
+package Controller
+
+import Data.IDataManager
+import Data.MemoryDataManager
+import Entity.Product
+
+class ProductController {
+    private var dataManager: IDataManager = MemoryDataManager
+
+    fun addProduct(product: Product) {
+        try {
+            dataManager.addProduct(product)
+        } catch (e: Exception) {
+            throw Exception("Error al agregar el producto")
+        }
+    }
+
+    fun removeOrder(id_product: Int) {
+        try {
+            dataManager.removeOrder(id_product.toString())
+        } catch (e: Exception) {
+            throw Exception("Error al eliminar el producto")
+        }
+    }
+
+    fun updateOrder(product: Product) {
+        try {
+            dataManager.updateProduct(product)
+        } catch (e: Exception) {
+            throw Exception("Error al actualizar el producto")
+        }
+    }
+}
